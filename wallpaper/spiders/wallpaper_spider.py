7e2c82a347dea41b91db11603500827741cac59c
749290765@qq.com <749290765@qq.com>
2017-04-28 10:24:40 +0800

    only one page

--- wallpaper/spiders/wallpaper_spider.py
+++ wallpaper/spiders/wallpaper_spider.py
@@ -12,7 +12,7 @@ class WallpaperSpider(CrawlSpider) : #CrawlSpider用来遍布抓取，通过rules来查找所
     name = "wallpaper"
     allowed_domains = ["zol.com.cn"]
     start_urls = [
-        "http://desk.zol.com.cn/meinv/1920x1080/hot_1.html",
+        "http://desk.zol.com.cn/bizhi/265_1288_2.html",
     ]
     headers = {
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
@@ -24,43 +24,8 @@ class WallpaperSpider(CrawlSpider) : #CrawlSpider用来遍布抓取，通过rules来查找所
             }
     
     def parse(self,response):
-        sel_root = response.xpath('//ul[@class="pic-list2  clearfix"]/li/a/@href')
-        for sel in sel_root:
-            mv_url = 'http://desk.zol.com.cn' + sel.extract()
-            print mv_url
-            yield scrapy.Request(
-                url = mv_url,
-                headers = self.headers,
-                callback = self.down_img,
-                dont_filter = True
-            )
-
-        for page in range(1,27):
-            start_url = "http://desk.zol.com.cn/meinv/1920x1080/hot_"+str(page)+".html"
-            yield scrapy.Request(
-                url = start_url,
-                headers = self.headers,
-                callback = self.parse_page,
-                dont_filter = True
-            )
-
-    def parse_page(self, response):
-        sel_root = response.xpath('//ul[@class="pic-list2  clearfix"]/li/a/@href')
-        for sel in sel_root:
-            mv_url = 'http://desk.zol.com.cn' + sel.extract()
-            print mv_url
-            yield scrapy.Request(
-                url = mv_url,
-                headers = self.headers,
-                callback = self.down_img,
-                dont_filter = True
-            )
-        
-        
-    def down_img(self, response):
-        print 'dddddddddddddddddddddddddddddddddddddddddddddddddddd'
-        sel = response.xpath('//ul[@id="showImg"]/li/a/img/@src')
-        print sel
         item = WallpaperItem()
-        item['imageurl'] = sel.extract()
+        sel = response.xpath('//ul[@id="showImg"]/li/a/img/@src').extract() + response.xpath('//ul[@id="showImg"]/li/a/img/@srcs').extract()
+        for i in range(len(sel)):
+            item['imageurl'][i] = sel[i].replace("144x90", "1920x1080",1)
         yield item
